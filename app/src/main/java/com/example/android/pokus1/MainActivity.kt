e8bc0385468363434f674112f24bb56fcf5bcd48
Milos.Simek@seznam.cz <Ripper@123>
2018-07-07 20:34:43 +0200

    Verze 0000001

--- app/src/main/java/com/example/android/pokus1/MainActivity.kt
+++ app/src/main/java/com/example/android/pokus1/MainActivity.kt
@@ -1,13 +1,61 @@
 package com.example.android.pokus1
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.view.Menu
+import android.view.MenuItem
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import com.example.android.pokus1.R
 
 class MainActivity : AppCompatActivity() {
 
+    var recyclerView: RecyclerView? = null
+    var layoutManager: RecyclerView.LayoutManager? = null
+    var adapter: RecyclerView.Adapter<*>? = null
+    var recycler: RecyclerAdapter = RecyclerAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        val CardInfo = CardInfo("Tri Sestry", "Na kovarne", "Rock", 2018, 4, true, 2000  )
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //val toolbar = findViewById(R.id.toolbar) as Toolbar
+        //setSupportActionBar(toolbar)
+
+        recyclerView = findViewById(R.id.recycler_view) as RecyclerView
+
+        layoutManager = LinearLayoutManager(this)
+        recyclerView?.layoutManager = layoutManager
+        val card = CardInfo("Metallica", "Kill em all", "Rock", 1988, 5, true, 10)
+        adapter = recycler
+        recycler.SetData(listOf(card))
+        recyclerView?.adapter = adapter
+        Thread.sleep(5000)
+        val card2 = CardInfo("Metallica", "Kill em all", "Rock", 1988, 5, true, 10)
+        recycler.SetData(listOf(card2))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+       // menuInflater.inflate(R.menu.menu_card_demo, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+       // val id = item.itemId
+
+
+//        return if (id == R.id.action_settings) {
+//            true
+//        }  else
+
+        Log.d("example", "cokoli")
+           return super.onOptionsItemSelected(item)
+
     }
 }
---
+++ app/src/main/java/com/example/android/pokus1/RecyclerAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.android.pokus1
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
+
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    public fun SetData(data:List<CardInfo>){
+        this.data =data
+        this.notifyDataSetChanged()
+    }
+
+    private var data: List<CardInfo> = ArrayList<CardInfo>()
+
+    //private val artists = arrayOf("Metallica", "Tri Sestry")
+    //, "Chapter Two", "Chapter Three", "Chapter Four", "Chapter Five", "Chapter Six", "Chapter Seven", "Chapter Eight")
+
+    //private val albums = arrayOf("Kill em all", "Na Kovarne")
+    //, "Item two details", "Item three details", "Item four details", "Item file details", "Item six details", "Item seven details", "Item eight details")
+
+    //private val genres = arrayOf("Rock", "Punk Rock")
+            //R.drawable.android_image_2, R.drawable.android_image_3, R.drawable.android_image_4, R.drawable.android_image_5, R.drawable.android_image_6, R.drawable.android_image_7, R.drawable.android_image_8)
+
+   class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var artist: TextView
+        var album: TextView
+        var genre: TextView
+
+        init {
+            artist = itemView.findViewById(R.id.artist) as TextView
+            album = itemView.findViewById(R.id.album) as TextView
+            genre = itemView.findViewById(R.id.genre) as TextView
+
+
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context)
+                .inflate(R.layout.releases_layout, viewGroup, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        viewHolder.artist.text = "Artist:" + data.get(i).Artist
+        viewHolder.album.text = "Album:" +data.get(i).Album
+        viewHolder.genre.text = "Genre:" +data.get(i).Genre
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
